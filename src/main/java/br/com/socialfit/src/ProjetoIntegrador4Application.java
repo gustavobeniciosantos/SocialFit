09a8637929df49ad341dc204ecb2cbb5941e22df
gustavobeniciosantos <gustavobeniciosantos@gmail.com>
2024-03-14 08:34:42 -0300

    Adição de pastas

---
+++ src/main/java/br/com/socialfit/src/ProjetoIntegrador4Application.java
@@ -0,0 +1,13 @@
+package br.com.socialfit.src;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegrador4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegrador4Application.class, args);
+	}
+
+}
