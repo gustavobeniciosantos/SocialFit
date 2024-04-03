6c2461765b232acc9f127b11a4cce48170d66f5b
gustavobeniciosantos <gustavobeniciosantos@gmail.com>
2024-04-03 16:02:53 -0300

    Serviço de envio de e-mail

--- src/main/java/br/com/socialfit/social_fit/service/CreateUser.java
+++ src/main/java/br/com/socialfit/social_fit/service/CreateUser.java
@@ -3,21 +3,31 @@ package br.com.socialfit.social_fit.service;
 import br.com.socialfit.social_fit.entity.User;
 import br.com.socialfit.social_fit.entity.UserRepository;
 import br.com.socialfit.social_fit.exeption.UserFoundExeption;
+import jakarta.mail.MessagingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 //Serve para realizar a regra de negócio para criação de um novo usuário
 @Service
 public class CreateUser {
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    EmailService emailService;
     public User executeRegister(User user){
 
         this.userRepository.findByUsernameOrEmailOrCPF(user.getUsername(), user.getEmail(), user.getCPF()).ifPresent((users) -> {
             throw new UserFoundExeption();
         });
+        try {
+            emailService.enviarEmailConfirmacao(user.getEmail());
+        } catch (MessagingException | IOException e) {
+            // Trate exceções de envio de e-mail aqui
+            e.printStackTrace();
+        }
         return this.userRepository.save(user);
-
     }
 
 }
---
+++ src/main/java/br/com/socialfit/social_fit/service/EmailService.java
@@ -0,0 +1,37 @@
+package br.com.socialfit.social_fit.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+@Service
+
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+    public void enviarEmailConfirmacao(String emailDestinatario) throws MessagingException, IOException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+
+        helper.setTo(emailDestinatario);
+        helper.setSubject("Confirmação de Conta");
+
+        ClassPathResource resource = new ClassPathResource("templates/confirmation.html");
+        String htmlContent = StreamUtils.copyToString(resource.getInputStream(), Charset.defaultCharset());
+
+
+        helper.setText(htmlContent, true);
+
+        javaMailSender.send(mimeMessage);
+    }
+}
